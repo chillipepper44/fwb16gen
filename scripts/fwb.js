8f58017c9dda9e9ce9f4359b80d17be89bed6fc6
chillipepper44 <prikthaipo@gmail.com>
2025-06-13 17:20:27 +0700

    Update fwb.js

--- scripts/fwb.js
+++ scripts/fwb.js
@@ -112,9 +112,9 @@ function generateFWB() {
 
   msg += `CVD/THB/PP/PP/NVD/NCV/XXX\n`;
   msg += `RTD/1/P${pcs}/K${weight}/CQ/W${cweight}/R1/T${pcs}\n`;
-  msg += `OTH/P/AWA1\n`;
-  msg += `/P/AWC1\n`;
-  msg += `PPD/WT${pcs}/TX1/CT${pcs}\n`;
+  msg += `OTH/P/AWC1\n`;
+  msg += `PPD/WT${pcs}\n`;
+  msg += `/OA1/OC1/CT${pcs}\n`;
   msg += `ISU/${$("flightDate")}/${$("dep")}\n`;
 
   document.getElementById("output").innerText = msg;
